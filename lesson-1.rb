d971c53c3db89699392b1fbdb22939758d7dc2e2
Sergey Golovin <golovim@gmail.com>
2014-09-18 22:57:09 +0400

    lesson-1.rb was added

---
+++ lesson-1.rb
@@ -0,0 +1,25 @@
+#Numbers
+num1 = 1
+num2 = 2.0
+num3 = 0b0010
+
+#Strings
+string1 = "String"
+string2 = 'String' 
+
+#Boolean
+bool1 = true
+bool2 = false
+
+#Symbols
+symbol1 = :symbol_one
+symbol2 = :bymbol_two
+
+#Arrays
+array1 = []
+array2 = [1,2,3,4]
+array3 = ["one", "two", "three"]
+
+#Hashes
+hash1 = {:one => "one_value", :two => "two_value"}
+hash2 = {:one => 1, :two => 2}
