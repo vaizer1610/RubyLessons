08f573679d305c4b934f580de6afbb6d5c766d67
Sergey Golovin <golovim@gmail.com>
2014-09-19 22:06:13 +0400

    if, unless and case statemets were added to a lesson-1

--- lesson-1.rb
+++ lesson-1.rb
@@ -23,3 +23,42 @@ array3 = ["one", "two", "three"]
 #Hashes
 hash1 = {:one => "one_value", :two => "two_value"}
 hash2 = {:one => 1, :two => 2}
+
+#conditional structures
+
+#if
+if 1 < 2
+    puts "two is greater that one"
+elsif 2 < 3
+    puts "three is greater that two"
+else
+    puts "something else"
+end
+
+#unless
+unless 1 > 2
+    puts "What?! O_o"
+else
+    puts "That was a nice joke :)"
+end
+
+
+#If or unless statement after an expression
+puts "Print this if 10 is greater that 5" if 10 > 5
+
+puts "Print this unless 5 is not greater that 10" unless 5 > 10
+
+#case statement
+age =  5
+case age
+when 0 .. 2
+    puts "baby"
+when 3 .. 6
+     puts "little child"
+when 7 .. 12
+     puts "child"
+when 13 .. 18
+     puts "youth"
+else
+     puts "adult"
+end
