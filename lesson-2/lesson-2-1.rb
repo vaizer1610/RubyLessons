3ace0988192b8694b95733ac027d3e3bc11693a5
Sergey Golovin <golovim@gmail.com>
2014-09-28 21:54:04 +0400

    lesson 2 was added

---
+++ lesson-2/lesson-2-1.rb
@@ -0,0 +1,19 @@
+#functions
+
+def fun1
+	puts "Hello world"
+end
+
+def fun2 name
+	puts "Hello #{name}"
+end
+
+def fun3 (firstName, lastName)
+	puts "Hello #{firstName} #{lastName}"
+end
+
+def fun4 (firstName = "John", lastName = "Doe")
+	puts "Hello #{firstName} #{lastName}"
+end
+
+
